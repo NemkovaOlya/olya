680a0691dfaac694c1cfbd33437c665f527489d4
NemkovaOlya <145974225+NemkovaOlya@users.noreply.github.com>
2023-09-25 11:36:27 +0000

    name

---
+++ name.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class name {
+    public static void main(String[] args) {
+        String name;
+        int age;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What is your name?");
+        name = sc.nextLine();
+        System.out.println("How old are you?");
+        age = sc.nextInt();
+    System.out.println("Hello, " + name);
+        System.out.println("you are " + age);
+        }
+        
+        
+        
+    }
+    
+
