3fd37a4b73f32863e9c1235c592bd63b39262398
NemkovaOlya <145974225+NemkovaOlya@users.noreply.github.com>
2023-09-25 14:13:27 +0000

    year

--- name.java
+++ name.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 
 public class name {
-    public static void main(String[] args) {
+    
         String name;
         int age;
-
+public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("What is your name?");
         name = sc.nextLine();
---
+++ year.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class year {
+    public static void main(String[] args) {{
+        int year;
+        int century;
+        Scanner sc = new Scanner(System.in);
+        System.out.println ("What year?");
+year = sc.nextInt();
+
+        
+    
+century = ((int) year/100);
+System.out.println("centry " + century);
+
+
+
+        
+    }}
+
+}
